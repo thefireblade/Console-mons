37d6f0a5ee210692f9a9ce3a3928a114e08eb35d
thefireblade <jhuang6625@bths.edu>
2018-03-12 11:20:53 -0400

    Debugging

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ import cx_Freeze
 from cx_Freeze import setup, Executable
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
-buildOptions = dict(packages = [], 
+'''buildOptions = dict(packages = [], 
 	excludes = [],
     includes = ["Trainer", "api"],
-    include_files = [])
+    include_files = []) '''
 setup(
     version = '0.1',
     name = 'Pokemon RPG : ALPHA',
     description = 'A Pokemon Parody Game created by JASON HUANG',
-    options = dict(build_exe = buildOptions),
+    #options = dict(build_exe = buildOptions),
     executables = [Executable("driver.py", base = base)]
     )
 
