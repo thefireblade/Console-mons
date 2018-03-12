9e1e62c832415030fc7e94ad6dfa43b62b638884
thefireblade <jhuang6625@bths.edu>
2018-03-12 11:13:44 -0400

    Making more changes

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ Created on Mar 11, 2018
 '''
 import cx_Freeze
 from cx_Freeze import setup, Executable
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+buildOptions = dict(packages = [], 
+	excludes = [],
+    includes = ["Trainer", "api"],
+    include_files = [])
 setup(
     version = '0.1',
     name = 'Pokemon RPG : ALPHA',
-    description = 'A Pokemon Parody Game created by JASON HUANG'
-    options = {'build_exe' :{'packages': ['api', 'Trainer']} },
-    executables = [Executable("driver.py",)]
+    description = 'A Pokemon Parody Game created by JASON HUANG',
+    options = dict(build_exe = buildOptions),
+    executables = [Executable("driver.py", base = base)]
     )
 
