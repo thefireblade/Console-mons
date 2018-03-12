0afb0437baffc42635b55094bff068894a6664c8
thefireblade <jhuang6625@bths.edu>
2018-03-12 12:15:26 -0400

    Adding compatibility to python 3

--- driver.py
+++ driver.py
@@ -9,7 +9,8 @@ from api import getPoke
 import random
 from Trainer import Pokemon
 import pickle
-
+try: input = raw_input
+except NameError: pass
     
 def printBreak():
     print("\n----------------------------------------------------")
@@ -18,9 +19,9 @@ def continueJourney(trainer):
     while(q) :
         print("\nCurrent Journey:")
         print(trainer.toString())
-        option = int(raw_input("\nOptions:\n(1)Walk Around\n(2)Next Area\n(3)Shop\n(4)Talk to NPCs\n(5)Poke Center\n(6)PVP\n(7)Save\nMy Choice:"))
+        option = int(input("\nOptions:\n(1)Walk Around\n(2)Next Area\n(3)Shop\n(4)Talk to NPCs\n(5)Poke Center\n(6)PVP\n(7)Save\nMy Choice:"))
         while(not option >=1 or not option <= 7 ):
-            option = int(raw_input("\nOptions:\n(1)Walk Around\n(2)Next Area\n(3)Shop\n(4)Talk to NPCs\n(5)Poke Center\n(6)PVP\n(7)Save\nMy Choice:"))
+            option = int(input("\nOptions:\n(1)Walk Around\n(2)Next Area\n(3)Shop\n(4)Talk to NPCs\n(5)Poke Center\n(6)PVP\n(7)Save\nMy Choice:"))
         if(option == 1):
             pokie = Pokemon(getPoke(random.randint(1,802)), random.randint(1,100))
             print("\nA random pokemon spawned: " + pokie.toString())
@@ -44,8 +45,8 @@ def continueJourney(trainer):
             print("\nSaving and exiting...")
             q = False
 def startJourney():
-    person = raw_input("\nWelcome to the world of pokemon! I'm professor Oak. WHOMST must you be?")
-    choice = int(raw_input("\n" + str(person) +", I'll hand over a starter to you. Choose between Bulbasaur, Squirtle, and Charmander (1,4,7)"))
+    person = input("\nWelcome to the world of pokemon! I'm professor Oak. WHOMST must you be?")
+    choice = int(input("\n" + str(person) +", I'll hand over a starter to you. Choose between Bulbasaur, Squirtle, and Charmander (1,4,7)"))
     starter = Pokemon(getPoke(choice), 5)
     print("\n" + starter.toString() + "\nhas been added to your team!")
     user = Trainer(str(person))
@@ -56,9 +57,9 @@ printBreak()
 print("\nWelcome to Pokemon RPG (ALPHA) By Jason Huang")
 printBreak()
 try:
-    response = int(raw_input("\nDo you want to load a file or start a new adventure (1 for load / 2 for new)"))
+    response = int(input("\nDo you want to load a file or start a new adventure (1 for load / 2 for new)"))
     while(not (response == 1 or response == 2)) :
-        response = int(raw_input("\nInvalid Response\nDo you want to load a file or start a new adventure (1 for load / 2 for new)"))
+        response = int(input("\nInvalid Response\nDo you want to load a file or start a new adventure (1 for load / 2 for new)"))
     if(response == 1) :
         pickleLoad = open('saveFile.pickle', 'rb')
         trainer = pickle.load(pickleLoad)
